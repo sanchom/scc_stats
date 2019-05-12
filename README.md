7af33fc48680eaf716ac587209c23e29e58f7db8
Sancho McCann <sanchom@gmail.com>
2019-05-11 21:15:39 -0700

    Updated stats script and README.

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
 This script gets the list of appeals for a given calendar year, then
 checks the docket for the list of interveners in each case. It prints
-a markdown-formatted output summarizing this info. The output, when
-rendered, looks like this:
-
-# 2018
-|Total decisions|Decisions with interventions|Appeals as of right|Appeals as of right with interventions|
-|---------------|----------------------------|-------------------|--------------------------------------|
-| 59 | 39 (66.10%) | 18 | 3 (16.67%) |
-
-|Neutral citation|Case name|From|Criminal?|As of right?|Interveners|Dockets|
-|----------------|---------|----|---------|------------|-----------|-------|
-| 2018 SCC 1 | [R. v. Seipp](https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/16957/index.do) | British Columbia | criminal |  | 1 |  [[1]](https://www.scc-csc.ca/case-dossier/info/parties-eng.aspx?cas=37513) |
-| 2018 SCC 2 | [Delta Air Lines Inc. v. Lukács](https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/16958/index.do) | Federal Court of Appeal |  |  | 4 |  [[1]](https://www.scc-csc.ca/case-dossier/info/parties-eng.aspx?cas=37276) |
-| 2018 SCC 3 | [Quebec (Commission des normes, de l’équité, de la santé et de la sécurité du travail) v. Caron](https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/16959/index.do) | Quebec |  |  | 8 |  [[1]](https://www.scc-csc.ca/case-dossier/info/parties-eng.aspx?cas=36605) |
-| 2018 SCC 4 | [Williams Lake Indian Band v. Canada (Aboriginal Affairs and Northern Development)](https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/16969/index.do) | Federal Court of Appeal |  |  | 8 |  [[1]](https://www.scc-csc.ca/case-dossier/info/parties-eng.aspx?cas=36983) |
-| 2018 SCC 5 | [R. v. Canadian Broadcasting Corp.](https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/16981/index.do) | Alberta | criminal |  | 2 |  [[1]](https://www.scc-csc.ca/case-dossier/info/parties-eng.aspx?cas=37360) |
+a markdown-formatted output summarizing this info. The output can be
+viewed [here](interveners-2009-2018.md).
 
 The `.pkl` file is a cache of info already downloaded from the SCC,
 but if you want to retrieve fresh data, just delete the `.pkl` file.
--- scc-stats.py
+++ scc-stats.py
@@ -7,13 +7,102 @@ import random
 
 logging.basicConfig(level=logging.INFO)
 
-def make_docket_links(docket_nos):
+def render_docket_links(docket_nos):
     url_base = 'https://www.scc-csc.ca/case-dossier/info/parties-eng.aspx?cas={}'
     link_text = ''
     for i, docket_no in enumerate(docket_nos, start=1):
         url = url_base.format(docket_no)
         link_text += ' [[{}]]({})'.format(i, url)
     return link_text
+
+def render_interveners(interveners):
+    x = '<ul>'
+    for i in interveners:
+        x += '<li>{}</li>'.format(i)
+    x += '</ul>'
+    return x
+
+def render_from(loc):
+    if not loc:
+        return 'None'
+    parts = loc.split(',')
+    translated_parts = []
+    for p in parts:
+        p = p.strip()
+        if p == 'Federal Court of Appeal':
+            translated_parts.append('FCA')
+        elif p == 'Quebec':
+            translated_parts.append('QC')
+        elif p == 'Ontario':
+            translated_parts.append('ON')
+        elif p == 'Manitoba':
+            translated_parts.append('MB')
+        elif p == 'Nova Scotia':
+            translated_parts.append('NS')
+        elif p == 'British Columbia':
+            translated_parts.append('BC')
+        elif p == 'Saskatchewan':
+            translated_parts.append('SK')
+        elif p == 'Alberta':
+            translated_parts.append('AB')
+        elif p == 'Northwest Territories':
+            translated_parts.append('NWT')
+        elif p == 'Prince Edward Island':
+            translated_parts.append('PE')
+        elif p == 'Yukon':
+            translated_parts.append('Y')
+        elif p == 'Nunavut':
+            translated_parts.append('NT')
+        elif p == 'Newfoundland and Labrador':
+            translated_parts.append('NL')
+        elif p == 'Canada':
+            translated_parts.append('Can')
+        elif p == 'New Brunswick':
+            translated_parts.append('NB')
+        elif p == 'Court Martial Appeal Court of Canada':
+            translated_parts.append('Ct Martial App Ct')
+        else:
+            raise Exception('unrecognized location: {}'.format(p))
+    return ', '.join(translated_parts)
+
+
+def print_markdown(decisions):
+    for year in range(2009, 2019):
+        print('# {}'.format(year))
+        num_cases = len([key for (key, val) in decisions.items() if val['publication_date'].year == year])
+        with_interventions = len([key for (key, val) in decisions.items() if val['publication_date'].year == year and val['interveners']])
+        as_of_right_appeals = len([key for (key, val) in decisions.items() if val['publication_date'].year == year and val['as_of_right']])
+
+        as_of_right_appeals_with_interventions = len([key for (key, val)
+                                                      in decisions.items() if val['publication_date'].year == year and
+                                                      val['as_of_right'] and val['interveners']])
+
+        print('|Total decisions|Decisions with interventions|Appeals as of right|Appeals as of right with interventions|')
+        print('|---------------|----------------------------|-------------------|--------------------------------------|')
+        print('| {} | {} ({:.2f}%) | {} | {} ({:.2f}%) |'.format(num_cases,
+                                                       with_interventions,
+                                                       100 * with_interventions / num_cases, as_of_right_appeals,
+                                                       as_of_right_appeals_with_interventions,
+                                                       100 * as_of_right_appeals_with_interventions / as_of_right_appeals))
+        print()
+        print('|Citation|From|Criminal?|As of right?|Interveners|Dockets|')
+        print('|---------|----|---------|------------|-----------|-------|')
+        for key, v in [(key, val) for (key, val) in decisions.items() if val['publication_date'].year == year]:
+            print('| _[{}]({})_, {} | {} | {} | {} | {} | {} |'.format(
+                v['title'], v['decision_url'], key, render_from(v['appeal_from']),
+                'criminal' if v['criminal'] else '', 'as of right' if v['as_of_right'] else '',
+                render_interveners(v['interveners']) if len(v['interveners']) else '',
+                render_docket_links(v['docket_no'])))
+        print()
+        print()
+
+def print_tsv(decisions):
+    for key, v in decisions.items():
+        print('{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
+            key, v['title'], v['decision_url'], v['appeal_from'],
+            'criminal' if v['criminal'] else '', 'as of right' if v['as_of_right'] else '',
+            len(v['interveners']) if len(v['interveners']) else ''))
+
 
 scc = CachedSCC('scc-cache.pkl')
 decisions = {}
@@ -34,30 +123,4 @@ for c in case_list:
                                 'as_of_right': d['as_of_right'],
                                 'interveners': list(set(d['interveners']))}
 
-for year in range(2009, 2019):
-    print('# {}'.format(year))
-    num_cases = len([key for (key, val) in decisions.items() if val['publication_date'].year == year])
-    with_interventions = len([key for (key, val) in decisions.items() if val['publication_date'].year == year and val['interveners']])
-    as_of_right_appeals = len([key for (key, val) in decisions.items() if val['publication_date'].year == year and val['as_of_right']])
-
-    as_of_right_appeals_with_interventions = len([key for (key, val)
-                                                  in decisions.items() if val['publication_date'].year == year and
-                                                  val['as_of_right'] and val['interveners']])
-                                                  
-    print('|Total decisions|Decisions with interventions|Appeals as of right|Appeals as of right with interventions|')
-    print('|---------------|----------------------------|-------------------|--------------------------------------|')
-    print('| {} | {} ({:.2f}%) | {} | {} ({:.2f}%) |'.format(num_cases,
-                                                   with_interventions,
-                                                   100 * with_interventions / num_cases, as_of_right_appeals,
-                                                   as_of_right_appeals_with_interventions,
-                                                   100 * as_of_right_appeals_with_interventions / as_of_right_appeals))
-    print()
-    print('|Neutral citation|Case name|From|Criminal?|As of right?|Interveners|Dockets|')
-    print('|----------------|---------|----|---------|------------|-----------|-------|')
-    for key, v in [(key, val) for (key, val) in decisions.items() if val['publication_date'].year == year]:
-        print('| {} | [{}]({}) | {} | {} | {} | {} | {} |'.format(
-            key, v['title'], v['decision_url'], v['appeal_from'],
-            "criminal" if v['criminal'] else '', 'as of right' if v['as_of_right'] else '',
-            len(v['interveners']) if len(v['interveners']) else '', make_docket_links(v['docket_no'])))
-    print()
-    print()
+print_markdown(decisions)
